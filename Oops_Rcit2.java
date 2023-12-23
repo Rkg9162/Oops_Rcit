6c03704f59bf64cb63051eb45e5425f50729a016
Rakesh Kumar <rakesh18146@gmail.com>
2023-12-23 16:42:20 +0530

    WAP enter any number from the use and find the squre of the number

---
+++ Oops_Rcit2.java
@@ -0,0 +1,15 @@
+
+//WAP enter any number from the use and find the squre of the number
+
+import java.util.Scanner;
+public class Oops_Rcit2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ente any number");
+        int Number = sc.nextInt();
+        System.out.println(Number*Number);
+        sc.close();
+        
+        
+    }
+}
