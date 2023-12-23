cebac9e106e7d3a05132b2b02031087b75ca9c84
Rakesh Kumar <rakesh18146@gmail.com>
2023-12-23 16:54:08 +0530

    WAP three number and thier sum

---
+++ Oops_Rcit3.java
@@ -0,0 +1,16 @@
+
+//WAP three number and thier sum
+import java.util.Scanner;
+public class Oops_Rcit3 {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number");
+        a =sc.nextInt();
+        b =sc.nextInt();
+        c =sc.nextInt();
+        System.out.println(a+b+c);
+        sc.close();
+        
+    }
+}
